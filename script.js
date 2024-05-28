df3e93e068840837442a7286fbbeffd791157656
Misgar-Numan <numanasgar10@gmail.com>
2024-05-28 20:40:59 +0530

    Add Dropdown functionality

--- script.js
+++ script.js
@@ -6,20 +6,28 @@ form.addEventListener("submit", function (e) {
   const height = parseFloat(document.querySelector("#height").value);
   const weight = parseFloat(document.querySelector("#weight").value);
   const results = document.querySelector("#results");
-  const unitDropdown = document.querySelector("#unit").value;
+  const heightDropdown = document.querySelector("#getHeight").value;
+  const weightDropdown = document.querySelector("#getWeight").value;
 
   if (isNaN(height) || height <= 0 || isNaN(weight) || weight <= 0) {
     results.innerHTML = "Please enter valid height and weight values.";
   } else {
     let heightInMeters;
+    let weightInKg;
 
-    if (unitDropdown === "feet") {
+    if (heightDropdown === "feet") {
       heightInMeters = height * 0.3048; // Convert height from feet to meters
     } else {
       heightInMeters = height / 100; // Convert height from centimeters to meters
     }
 
-    const bmi = (weight / (heightInMeters * heightInMeters)).toFixed(2);
+    if(weightDropdown === "Pounds") {
+      weightInKg = weight * 0.45359237; // convert weight from pounds to kg
+    } else{
+      weightInKg = weight; // Don't change weight
+    }
+
+    const bmi = (weightInKg / (heightInMeters * heightInMeters)).toFixed(2);
 
     if (bmi < 18.5) {
       results.innerHTML = `${bmi} - Underweight`;
@@ -33,22 +41,6 @@ form.addEventListener("submit", function (e) {
     }
   }
 });
-
-function changeUnit() {
-  let unitDropdown = document.getElementById("unit");
-  let heightInput = document.getElementById("height");
-  let unitLabel = document.querySelector("label[for='height']");
-
-  let heightValue = parseFloat(heightInput.value);
-
-  if (unitDropdown.value === "feet") {
-    heightInput.value = (heightValue / 30.48).toFixed(2);
-    unitLabel.textContent = "Height in Feet:";
-  } else {
-    heightInput.value = (heightValue * 30.48).toFixed(2);
-    unitLabel.textContent = "Height in CM:";
-  }
-}
 
 const shareButton = document.getElementById("share-button");
 shareButton.addEventListener("click", shareBMI);
